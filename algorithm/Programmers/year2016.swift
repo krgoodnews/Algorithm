dc799818988f30344c6cba30b423be324425fcf0
Goodnews <krgoodnews@gmail.com>
2018-09-11 14:07:24 +0900

    Programmers: 2016년

---
+++ algorithm/Programmers/year2016.swift
@@ -0,0 +1,16 @@
+// 프로그래머스 - 2016년
+// https://programmers.co.kr/learn/courses/30/lessons/12901
+
+func solution(_ a:Int, _ b:Int) -> String {
+	let dayStrArr = ["THU","FRI","SAT", "SUN","MON","TUE","WED"]
+	let month = [0, 31, 29, 31, 30, 31, 30, 31,31,30,31,30,31]
+	
+	var day = 0
+	for i in 0..<a {
+		day += month[i]
+	}
+	day += b
+	return dayStrArr[day%7]
+}
+
+print(solution(5, 24))
