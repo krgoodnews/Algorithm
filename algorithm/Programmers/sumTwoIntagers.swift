bb666a16d2acdce6abcc23bba3149079526f9efd
Goodnews <krgoodnews@gmail.com>
2018-09-11 14:07:17 +0900

    Programmers: 두 정수 사이의 합

---
+++ algorithm/Programmers/sumTwoIntagers.swift
@@ -0,0 +1,13 @@
+// 두 정수 사이의 합
+
+func solution(_ a:Int, _ b:Int) -> Int64 {
+	var result = 0
+	
+	let minV = min(a, b)
+	let maxV = max(a, b)
+	for i in minV...maxV {
+		result += i
+	}
+	
+	return Int64(result)
+}
