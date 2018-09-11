16c1c86b6a500c2f219c16cdf1fd2228a72d8639
Goodnews <krgoodnews@gmail.com>
2018-09-11 14:06:50 +0900

    Programmers: Descending

---
+++ algorithm/Programmers/descending.swift
@@ -0,0 +1,16 @@
+// 정수 내림차순으로 배치하기
+
+func solution(_ n:Int64) -> Int64 {
+	var arr = Array(String(n)).sorted().reversed()
+	
+	var result = ""
+	for ch in arr {
+		result.append(ch)
+	}
+	
+	
+	return Int64(result)!
+	
+}
+
+
