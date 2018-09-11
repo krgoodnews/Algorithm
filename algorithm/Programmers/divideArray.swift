0225757d088c54d153882a1999bbcce253845dce
Goodnews <krgoodnews@gmail.com>
2018-09-11 14:07:08 +0900

    Programmers: divideArray

---
+++ algorithm/Programmers/divideArray.swift
@@ -0,0 +1,20 @@
+// 프로그래머스 - 나누어 떨어지는 숫자 배열
+// https://programmers.co.kr/learn/courses/30/lessons/12910?language=swift
+
+func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+	var result = [Int]()
+	
+	for v in arr {
+		if v%divisor == 0 {
+			result.append(v)
+		}
+	}
+	
+	result.sort()
+	
+	if result.count == 0 {
+		result.append(-1)
+	}
+	
+	return result
+}
