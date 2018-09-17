a83f99c1e0e105dfcf62018b54c3e2ec8a215961
Goodnews <krgoodnews@gmail.com>
2018-09-17 16:02:21 +0900

    init CppAlgorithm

---
+++ CppAlgorithm/CppAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppAlgorithm
+//
+//  Created by Goodnews on 17/09/2018.
+//  Copyright © 2018 krgoodnews. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
