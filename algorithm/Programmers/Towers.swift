ea3990c0fee12967dd8b323e038e6f5f3d249d15
Yunsu Guk <krgoodnews@gmail.com>
2020-04-24 21:14:50 +0900

    프로그래머스 연습 > 스택/큐 > 탑

---
+++ algorithm/Programmers/Towers.swift
@@ -0,0 +1,24 @@
+//
+//  Towers.swift
+//  algorithm
+//
+//  Created by Yunsu Guk on 2020/04/24.
+//  Copyright © 2020 krgoodnews. All rights reserved.
+//
+
+import Foundation
+
+func receiveTower(leftTowers: [Int], height: Int) -> Int {
+    for (i, towerHeight) in leftTowers.enumerated().reversed() {
+        if height < towerHeight { return i + 1 }
+    }
+    return 0
+}
+
+func solution(_ heights:[Int]) -> [Int] {
+    var result: [Int] = []
+    for i in 0..<heights.count {
+        result.append(receiveTower(leftTowers: Array(heights[0..<i]), height: heights[i]))
+    }
+    return result
+}
